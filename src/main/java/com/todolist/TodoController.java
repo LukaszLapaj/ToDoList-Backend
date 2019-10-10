1fbfa3e1ea2000bb91821dfdb590ed2d751d17ee
mofblaster <mastero9017@gmail.com>
2019-10-10 21:11:44 +0200

    Add GET, POST, DELETE methods

--- src/main/java/com/todolist/TodoController.java
+++ src/main/java/com/todolist/TodoController.java
@@ -3,12 +3,35 @@ package com.todolist;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/tasks")
 @AllArgsConstructor
 public class TodoController {
     private final TodoRepository repository;
+
+    @GetMapping("")
+    public List<Todo> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Todo get(@PathVariable long id) {
+        return repository.getOne(id);
+    }
+
+
+    @PostMapping("")
+    public void add(@RequestBody Todo task) {
+        repository.saveTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        repository.deleteById(id);
+    }
 
 
 }
